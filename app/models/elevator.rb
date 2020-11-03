075cdfac99a52dd199cce10c5017182d594a2711
kngBMermaid <louis.marmen.92@gmail.com>
2020-11-03 13:55:54 -0500

    Twilio working on elevator status change v2

--- app/models/elevator.rb
+++ app/models/elevator.rb
@@ -5,14 +5,14 @@ class Elevator < ApplicationRecord
     belongs_to :customer
     
     after_update do |e|
-        if e.status = 'intervention' 
-            account_sid = "ITSASECRET" # Your Test Account SID from www.twilio.com/console/settings
-            auth_token = "ADEADLYSECRET"   # Your Test Auth Token from www.twilio.com/console/settings
+        if e.status == 'intervention'
+            account_sid = "SECRETOFLIFE" # Your Test Account SID from www.twilio.com/console/settings
+            auth_token = "SECRETOFLIFE"   # Your Test Auth Token from www.twilio.com/console/settings
             
             @client = Twilio::REST::Client.new account_sid, auth_token
             message = @client.messages.create(
-                body: "An elevator has just changed status to intervention.",
-                to: "COVID19",    # Replace with your phone number
+                body: "The Elevator #{e.id} has just changed status to intervention.",
+                to: "SECRETOFLIFE",    # Replace with your phone number
                 from: "+16475608952")  # Use this Magic Number for creating SMS
             
             puts message.sid
