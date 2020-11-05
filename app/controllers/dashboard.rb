379839b4eca588f52153ffa24f100c5ff65873be
Jcoderre <joeysteam@outlook.com>
2020-11-04 21:22:10 -0500

    all data work

--- app/controllers/dashboard.rb
+++
@@ -1,5 +0,0 @@
-class DaashboardController < ApplicationController
-    def daashboard
-        p params[:controller]
-    end
-end
--- app/models/building.rb
+++ app/models/building.rb
@@ -10,6 +10,22 @@ class Building < ApplicationRecord
         "#{address_of_the_building}"
     end
     belongs_to :address
-   
 
+    def how_many_columns
+     how_many = 0
+     self.batteries.each do |battery|
+        how_many = how_many + battery.columns.size
+     end
+     how_many
+    end
+
+    def how_many_elevators
+        how_many = 0
+            self.batteries.each do |battery|
+                battery.columns.each do |column|
+                    how_many = how_many + column.elevators.size
+                end
+            end
+        how_many
+    end
 end
