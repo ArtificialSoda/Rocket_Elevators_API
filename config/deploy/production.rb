1bc2bbe2fc60aa4d8c93865b69fdd059ec41c1f0
artififcialsoda <dimitrov.fabien@gmail.com>
2020-11-05 13:09:16 -0500

    Change deployment app name and repo URL

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -8,7 +8,7 @@ server "99.79.123.96", user: "ubuntu", roles: %w{app db web}
 # server "db.example.com", user: "deploy", roles: %w{db}
 app = ENV['APP']
 if app.nil? or app.empty?
-  app = "Trevor_Kitchen_team2" 
+  app = "Fabien_Dimitrov_Team4" 
 end
 set :application, app
 set :rails_env, "development"
